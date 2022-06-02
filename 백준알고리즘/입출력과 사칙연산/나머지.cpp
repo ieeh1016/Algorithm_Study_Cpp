84f72331abb840209e0a84160ed573ef93ad2366
YW <ieeh0122@naver.com>
2022-06-02 20:52:18 +0900

    10430

---
+++ 백준알고리즘/입출력과 사칙연산/나머지.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    cin >> a >> b >> c;
+
+    cout << (a + b) % c << endl;
+    cout << ((a % c) + (b % c)) % c << endl;
+    cout << ( a * b ) % c << endl;
+    cout << ((a % c) * (b % c)) % c << endl;
+
+    return 0;
+}
