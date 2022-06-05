99cf43cc7c777a7bbdecd3f7d8dc8937e766ec24
YW <ieeh0122@naver.com>
2022-06-05 22:53:52 +0900

    1330

---
+++ 백준알고리즘/조건문/두 수 비교하기.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{	
+	int A, B;
+		cin>> A>> B;
+		if (A <= 10000 && A >= -10000 &&
+			B <= 10000 && B >= -10000)
+		{
+			if (A > B)
+			{
+				cout << ">";
+			}
+			else if (A < B)
+			{
+				cout << "<";
+			}
+
+			else
+				cout << "==";
+
+		}
+
+}
